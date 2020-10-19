9952a0aa240efefc6b3149d54ec4c63d2a1130fa
EpicDoomSir <eitands@gmail.com>
2020-10-19 16:38:39 -0400

    added layout for our seed file data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,14 @@
 User.destroy_all
+Ship.destroy_all
+Game.destroy_all
+Asteroid.destroy_all
+
+User.create(username: '', email_address: '', password: '', high_score: '')
+User.create(username: '', email_address: '', password: '', high_score: '')
+
+Ship.create(user_id: '', game_id: '', hp: '', scores: '')
+Ship.create(user_id: '', game_id: '', hp: '', scores: '')
+
+Game.create(difficulty: '')
+
+Asteroid.create(game_id: '', hp: '', size: '')
