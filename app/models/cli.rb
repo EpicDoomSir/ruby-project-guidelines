03e803935739b7f1233efaaf076c3cc2f08707bf
techtwins <gshevaughn@gmail.com>
2020-10-20 16:07:24 -0400

    finished sigin and register options, and centered some text

--- app/models/cli.rb
+++ app/models/cli.rb
@@ -5,18 +5,19 @@ class CLI
     def initialize
         @prompt = TTY::Prompt.new
         @font = TTY::Font.new(:doom)
+        @pastel = Pastel.new
         @user = []
     end
 
     def welcome
-        puts @font.write("ASTEROIDS", letter_spacing: 4) # displays game title
+        puts @pastel.magenta.bold(@font.write("ASTEROIDS".center(30), letter_spacing: 4)) # displays game title
         sleep(1)                          # input a delay before displaying the menu/selection screen below
 
-        selection = @prompt.select('Please select one of the options below:') do |menu|
-            menu.choice 'Create Account', 1
-            menu.choice 'Sign In', 2
-            menu.choice 'Exit', 3
-        end
+        selection = @prompt.select('Please select one of the options below:'.center(180))do |menu|
+            menu.choice 'Create Account'.center(180), 1
+            menu.choice 'Sign In'.center(180), 2
+            menu.choice 'Exit'.center(180), 3
+    end
         if selection == 1
             self.new_acc
         elsif selection == 2
@@ -37,9 +38,32 @@ class CLI
             puts "That username is already taken, please try again."
             self.welcome
         end
+        self.menu
     end
 
     def login_acc
+        system("clear")
+        u_input = @prompt.ask('Please enter a username:', required: true)
+        e_user = User.find_by(username: u_input)
+        system("clear")
+        if e_user == nil
+            puts "User not found, try again!"
+            self.login_acc
+        else pass = @prompt.mask('Please enter a password:', required: true)
+            login = User.find_by(username: u_input, password: pass)
+            system("clear")
+            if login == nil
+                puts "Wrong password, try again!"
+                self.login_acc
+            else
+                puts "Welcome back #{e_user.username}!"
+            end
+        end
+        self.menu
+    end
+
+    def menu
+        
     end
 
     def close
